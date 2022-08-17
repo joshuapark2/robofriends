325f03229785fdbadb59c00829fd11b607fddfee
Joshua Park <88602267+ironkun1309@users.noreply.github.com>
2022-08-17 01:30:30 -0400

    Updates

---
+++ precache-manifest.89f1d750f084887cc69039eced03b7e9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "cdc063721cce6b62d1cb",
+    "url": "/robofriends/static/css/main.57497b50.chunk.css"
+  },
+  {
+    "revision": "cdc063721cce6b62d1cb",
+    "url": "/robofriends/static/js/main.66c63469.chunk.js"
+  },
+  {
+    "revision": "c85c82f5ba82327a3218",
+    "url": "/robofriends/static/js/runtime~main.f7dcbb18.js"
+  },
+  {
+    "revision": "df56cc7a0acd494bb19e",
+    "url": "/robofriends/static/css/2.ea8c63dd.chunk.css"
+  },
+  {
+    "revision": "df56cc7a0acd494bb19e",
+    "url": "/robofriends/static/js/2.05b6de8d.chunk.js"
+  },
+  {
+    "revision": "9c19088db84563606874",
+    "url": "/robofriends/static/js/3.5526bd81.chunk.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "f694925900b6883e01ec555f45d281e2",
+    "url": "/robofriends/index.html"
+  }
+];
